fb264daa9d309f4a37bc2023cf2d26a023715fc6
ertugrulvural <ertugrul.vural.07@gmail.com>
2024-12-09 23:39:04 +0300

    contact css kodları about altına taşındı

--- js/script.js
+++ js/script.js
@@ -42,33 +42,3 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
     AOS.init();
 });
-
-// Slayt geçiş işlevselliği
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
-
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-
-function showSlide(index) {
-    if (index >= totalSlides) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else {
-        currentIndex = index;
-    }
-
-    const slider = document.querySelector('.slider');
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-prevBtn.addEventListener('click', () => showSlide(currentIndex - 1));
-nextBtn.addEventListener('click', () => showSlide(currentIndex + 1));
-
-// Mini butonlara tıklama işlevselliği
-const miniButtons = document.querySelectorAll('.slider-controls button');
-miniButtons.forEach((btn, index) => {
-    btn.addEventListener('click', () => showSlide(index));
-});
